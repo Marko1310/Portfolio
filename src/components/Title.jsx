3ac3f4665ea62559c0c0d996225ef0fed53155f1
Marko1310 <marko.cabo13@gmail.com>
2023-03-15 14:18:07 +0100

    changing dark mode functionality across the page

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ const Title = ({ children, id }) => {
   return (
     <h1
       id={id && id}
-      className="text-2xl font-bold underline underline-offset-8 decoration-4 mb-5 text-stone-990 "
+      className="text-2xl font-bold underline underline-offset-8 decoration-4 mb-5 text-stone-990 dark:text-white"
     >
       {children}
     </h1>
