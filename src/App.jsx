6970bae3a753fea351f04ad50d9d15cb0f44cf19
Marko1310 <marko.cabo13@gmail.com>
2023-03-15 13:25:43 +0100

    created contact form and footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ function App() {
   return (
     <div className="App">
       <Intro />
-      <Contact />
       <Portfolio />
       <Timeline />
       <Footer />
+      <Contact />
     </div>
   );
 }
