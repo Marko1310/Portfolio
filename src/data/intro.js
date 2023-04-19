ed3fac499713b531b3b2678d473727200114afda
Marko1310 <marko.cabo13@gmail.com>
2023-04-19 19:36:19 +0200

    text fix

--- src/data/intro.js
+++ src/data/intro.js
@@ -1,34 +1,34 @@
 export default [
   {
-    id:1,
-    imgUrl: "/assets/html.png",
+    id: 1,
+    imgUrl: '/assets/html.png',
   },
   {
-    id:2,
-    imgUrl: "/assets/css.png",
+    id: 2,
+    imgUrl: '/assets/css.png',
   },
   {
-    id:3,
-    imgUrl: "/assets/javascript.png",
+    id: 3,
+    imgUrl: '/assets/javascript.png',
+  },
+  // {
+  //   id:4,
+  //   imgUrl: "/assets/typescript.png",
+  // },
+  {
+    id: 5,
+    imgUrl: '/assets/react.png',
   },
   {
-    id:4,
-    imgUrl: "/assets/typescript.png",
+    id: 6,
+    imgUrl: '/assets/tailwind.png',
   },
   {
-    id:5,
-    imgUrl: "/assets/react.png",
+    id: 7,
+    imgUrl: '/assets/nodejs.png',
   },
   {
-    id:6,
-    imgUrl: "/assets/tailwind.png",
-  },
-  {
-    id:7,
-    imgUrl: "/assets/nodejs.png",
-  },
-  {
-    id:8,
-    imgUrl: "/assets/sql.png",
+    id: 8,
+    imgUrl: '/assets/sql.png',
   },
 ];
